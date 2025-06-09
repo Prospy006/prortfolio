c1a8abd76b1eb599362cf6f79fd4090f08c7653c
Prospy006 <74852993+Prospy006@users.noreply.github.com>
2025-06-09 18:45:10 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,4 @@
-
+window.addEventListener('init', () => {
+    const loadTime = performance.timing.loadEventEnd - performance.timing.navigationStart;
+    console.log(`Loaded in ${loadTime}ms`);
+});

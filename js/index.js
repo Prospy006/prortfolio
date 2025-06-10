423cc3ba9d983da948dd5cb57c8a71e55dd8b3e0
Prospy006 <74852993+Prospy006@users.noreply.github.com>
2025-06-10 13:57:51 +0000

    fade in

--- js/index.js
+++ js/index.js
@@ -1,9 +1,29 @@
-// nerd crap
+//https://tenor.com/moVX2QjMGtk.gif
+
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('web DOM CONTENT loaded');
-});
-window.addEventListener('load', () => {
-    console.log('site loaded');
+    const fadeElements = document.querySelectorAll('.fade-in-module');
+
+    const observerOptions = { //crap which fades in trigger
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('is-visible');
+                observer.unobserve(entry.target);
+            }
+            else {
+                entry.target.classList.remove('is-visible');
+            }
+        });
+    }, observerOptions);
+
+    fadeElements.forEach(element => {
+        observer.observe(element);
+    });
 });
 // mobile accessibility
 function updateResponsiveClass() {
